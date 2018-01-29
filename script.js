788e9cb8b94b9eb3ad9b4318d87181723afc3cb5
Mevith <mevitth@gmail.com>
2018-01-29 19:33:12 +0100

    Add two functions for player and computer choice

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
 function computerPlay() {
-    let choice = '';
+    let choice;
     
     let pick = Math.floor(Math.random() * 3);
 
-    if(pick === 0) {
-        choice = 'Rock';
-    } else if(pick === 1) {
-        choice = 'Paper';
-    } else if(pick === 2) {
-        choice = 'Scissors';
+    switch(pick) {
+        case 0:
+            choice = 'Rock';
+            break;
+        case 1:
+            choice = 'Paper';
+            break;
+        case 2:
+            choice = 'Scissors';
+            break;
     }
 
     return choice;
 }
+
+const computerSelection = computerPlay();
+
+function playerPlay() {
+    let input;
+    let query = prompt('Choose:\nrock, paper, scissors');
+
+    switch(query.toLowerCase()) {
+        case 'rock':
+            input = 'Rock';
+            break;
+        case 'paper':
+            input = 'Paper';
+            break;
+        case 'scissors':
+            input = 'Scissors';
+            break;
+        default:
+            prompt('Choose right option:\nrock, paper, scissors');
+    }
+    return input;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+// const playerSelection = 'rock';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection))
+
+
+
+
+
+
+
